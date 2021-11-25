9f121d8701dd3db3b05a326cf9972ef9ea8517ff
Joris Marhuenda <marhuendajoris@gmail.com>
2021-11-25 21:31:57 +0100

    finish all logic and control code

---
+++ Pierre_Feuille_Ciseaux/Controller/ResultController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultController.swift
+//  Pierre_Feuille_Ciseaux
+//
+//  Created by marhuenda joris on 25/11/2021.
+//
+
+import SwiftUI
+
+class ResultController: UIViewController {
+    @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var gameEngine: GameEngine?
+    var delegate: GameDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let receivedGE = gameEngine {
+            resultLabel.text = receivedGE.resultString
+            
+            if receivedGE.resultString == "Félicitations c'est gagné!!" && receivedGE.gameType == .HumanVsBot {
+                bannerImageView.image = UIImage(named: "wonboy")
+            } else {
+                bannerImageView.image = UIImage(named: "robot")
+            }
+        }
+    }
+    
+    @IBAction func playAgainButtonTouched(_ sender: Any) {
+        if let delegate = self.delegate, let gameEngine = self.gameEngine {
+            self.dismiss(animated: true, completion: nil)
+            delegate.didPlaySameGame(result: gameEngine)
+        }
+    }
+    
+    @IBAction func changeModeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
